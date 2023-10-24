28fe24a90223e50012e1e043dea451b6596f583d
Shamini <shaminisri12@gmail.com>
2023-10-24 05:44:06 +0530

    Working setup completed

--- cloud-config-server/src/main/java/com/microcredentials/ccs/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/microcredentials/ccs/CloudConfigServerApplication.java
@@ -2,10 +2,10 @@ package com.microcredentials.ccs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-//@EnableConfigServer
-//@EnableEurekaClient
+@EnableConfigServer
 public class CloudConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudConfigServerApplication.class, args);
